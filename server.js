26c844ca4de781a0123bd4522f22730b49b37722
A Bevers <alisabevers@gmail.com>
2023-05-04 20:05:33 -0400

    correctly linked the css to server.js file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, './Develop/public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './Develop/public/index.html'));
