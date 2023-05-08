f291498de5ebdb35b18eda01d9834d1dc6fa5e81
A Bevers <alisabevers@gmail.com>
2023-05-08 09:54:46 -0400

    rendered notes to the sidebar upon save, and added the delete function

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const db = require('./Develop/db/db.json');
-const fs = require('fs');
-const PORT = 3001;
+const {readFile, writeFile} = require('fs/promises');
+const PORT = process.env.PORT || 3001;
+const getNotes = () => {
+  return readFile('./Develop/db/db.json').then(rawNotes => [].concat(JSON.parse(rawNotes)))
+}
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, './Develop/public')));
 
@@ -19,44 +21,35 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(db);
-  // res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+  getNotes().then(notes => res.json(notes))
+  .catch(err => res.status(404).json(err))
 });
 
 app.post('/api/notes', (req, res) => {
-  const { title, text } = req.body;
-  if (title && text ) {
-    const newNote = {
-      title,
-      text,
-    };
-
-  fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
-    if(err) {
-      console.log(err);
-    } else {
-      const dataFromFile = JSON.parse(data);
-      dataFromFile.push(newNote);
-      const stringifiedData = JSON.stringify(dataFromFile);
-        fs.writeFile(`./Develop/db/db.json`, stringifiedData, (err) => err
-        ? console.log(error(err))
-        : console.log(`A new note called, "${newNote.title}" was written to the db.json file.`)
-        );
+  getNotes().then(oldNotes => {
+    const { title, text } = req.body;
+    if (title && text ) {
+      const newNote = {
+        title,
+        text,
+        id: Math.floor(Math.random()*10000).toString()
+      };
+      const noteArray = [...oldNotes, newNote];
+      writeFile("./Develop/db/db.json", JSON.stringify(noteArray)).then(() => res.json({
+        msg: "ok" }))
     }
   })
+})
 
-    const response = {
-      status: "success",
-      body: newNote,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
-
-  } else {
-    res.status(500).json('Error in posting your note');
-  }
-});
+app.delete('/api/notes/:id', (req, res) => {
+  getNotes().then(oldNotes => {
+    let filteredNotes = oldNotes.filter(note => note.id !== req.params.id);
+    console.log(filteredNotes);
+    writeFile("./Develop/db/db.json", JSON.stringify(filteredNotes))
+  })
+  .then(() => res.json({
+    msg: "ok" }))
+})
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
